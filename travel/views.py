951184f89bc0347b8ee1f4bad5f2b529ab108f3c
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-01-27 22:18:26 +0700

    change_view

--- travel/views.py
+++ travel/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    name = 'About us'
+
+    return render(request, 'about.html', {'name': name})
