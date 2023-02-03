1d78d02febbd78959d727652e7741c4f32943c45
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-03 12:08:17 +0700

    add_forms

--- cities/forms.py
+++ cities/forms.py
@@ -1,4 +1,18 @@
 from django import forms
 
+from cities.models import City
+
+
 class HtmlForm(forms.Form):
     name = forms.CharField(label='Город')
+
+class CityForm(forms.ModelForm):    # Проверка соблюдения уникальности
+    name = forms.CharField(label='Город', widget=forms.TextInput(attrs={  #Изменение формы
+        'class': 'form-control',
+        'placeholder': 'Введите название города'
+    }))     #
+
+
+    class Meta:                     # Поля которые отображаем
+        model = City
+        fields = ('name',)
