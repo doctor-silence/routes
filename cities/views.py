f4d2822f534e190c59e80a46984d542dbd8c0f77
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-03 12:06:26 +0700

    create_city

--- cities/views.py
+++ cities/views.py
@@ -1,26 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView',
+    'home', 'CityDetailView', 'CityCreateView'
 )
 
 
 def home(request, pk=None):
     if request.method == 'POST':
-        print(request.POST)
+        form = CityForm(request.POST)
+        if form.is_valid():   #Проверяем что форма валидна
+            print(form.cleaned_data)  #Получаем нужные данные из формы
+            form.save()             #Сохраняем полученные данные
     #if pk:
         #city = City.object.filter(id=pk).first  #Что-бы не получить ошибку Does not Exist используем filter
         #city = get_object_or_404(City, id=pk)
 
         #context = {'object': city}
         #return render(request, 'cities/detail.html', context)
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home') # После создания переадресация
