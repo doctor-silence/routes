8d80c10c9c64b9f8512201a0f8315c523dd01291
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-05 22:52:09 +0700

    add_new_model

--- cities/models.py
+++ cities/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class City(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Город')
@@ -11,4 +13,7 @@ class City(models.Model):
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
         ordering = ['name']
+
+   # def get_absolute_url(self):
+    #    return reverse('cities:detail', kwargs={'pk': self.pk})     #После добавления нового города, переход на страницу города
 
