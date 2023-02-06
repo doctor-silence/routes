f03cbb836f5fab1978ca5ab6c1dad7d7a453c3ac
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-06 22:55:06 +0700

    add_paginator_back

--- cities/urls.py
+++ cities/urls.py
@@ -4,7 +4,8 @@ from cities.views import *
 from cities.views import CityDeleteView
 
 urlpatterns = [
-    path('', home, name='home'),
+    #path('', home, name='home'),
+    path('', CityListView.as_view(), name='home'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),  # Может быть целое число которое поместим в pk
     path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
