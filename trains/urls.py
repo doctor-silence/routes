5565b9640b18386c7685dd92aca057753ba3fd6d
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-09 19:15:57 +0700

    add_url_trains

--- trains/urls.py
+++ trains/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from cities.views import *
-from cities.views import CityDeleteView
+from trains.views import *
+#from trains.views import TrainDeleteView
 
 urlpatterns = [
     #path('', home, name='home'),
-    path('', CityListView.as_view(), name='home'),
-    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),  # Может быть целое число которое поместим в pk
-    path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
-    path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
-    path('add/', CityCreateView.as_view(), name='create'),
+    path('', TrainListView.as_view(), name='home'),
+    # path('detail/<int:pk>/', TrainDetailView.as_view(), name='detail'),  # Может быть целое число которое поместим в pk
+    # path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
+    # path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
+    # path('add/', TrainCreateView.as_view(), name='create'),
 ]
