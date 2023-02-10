7463ce9964ab2c02e08915b6817d5ae6b7df22d1
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-10 22:26:54 +0700

    add_trains_url

--- trains/urls.py
+++ trains/urls.py
@@ -6,8 +6,8 @@ from trains.views import *
 urlpatterns = [
     #path('', home, name='home'),
     path('', TrainListView.as_view(), name='home'),
-    # path('detail/<int:pk>/', TrainDetailView.as_view(), name='detail'),  # Может быть целое число которое поместим в pk
+    path('detail/<int:pk>/', TrainDetailView.as_view(), name='detail'),  # Может быть целое число которое поместим в pk
     # path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
     # path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
-    # path('add/', TrainCreateView.as_view(), name='create'),
+    path('add/', TrainCreateView.as_view(), name='create'),
 ]
