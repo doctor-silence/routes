d03eb92254a2a1efa56d204afa1352642bbe2559
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-11 17:38:47 +0700

    add_trains_url

--- trains/urls.py
+++ trains/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from trains.views import *
-#from trains.views import TrainDeleteView
+from trains.views import TrainUpdateView, TrainDeleteView
 
 urlpatterns = [
     #path('', home, name='home'),
     path('', TrainListView.as_view(), name='home'),
     path('detail/<int:pk>/', TrainDetailView.as_view(), name='detail'),  # Может быть целое число которое поместим в pk
-    # path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
-    # path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
+    path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
     path('add/', TrainCreateView.as_view(), name='create'),
 ]
