9b4aa7226a335cee2794433222bd254ff7cec682
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-16 13:38:35 +0700

    add_dfs_func

--- routes/utils.py
+++ routes/utils.py
@@ -1,6 +1,17 @@
 from trains.models import Train
 
 
+def dfs_paths(graph, start, goal):
+   stack = [(start, [start])]
+   while stack:
+       (vertex, path) = stack.pop()
+       if vertex in graph.keys():
+           for next_ in graph[vertex] - set(path):
+               if next_ == goal:
+                    yield path + [next_]
+               else:
+                   stack.append((next_, path + [next_]))
+
 def get_graph(qs):
     graph = {}
     for q in qs:
@@ -10,6 +21,14 @@ def get_graph(qs):
 
 #Функция поиска маршрута
 def get_routes(request, form) -> dict:
+    context = {'form': form}
     qs = Train.objects.all()
     graph = get_graph(qs)
-
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    travelling_time = data['travelling_time']
+    all_ways = dfs_paths(graph, from_city.id, to_city.id)
+    if not len(list(all_ways)):
+        raise ValueError('Маршрута, удовлетворяющего условия не существует ')
+    return context
