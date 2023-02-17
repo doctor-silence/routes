e4ff6eaec1a906fe8b836ae66cdb66484dd5ca29
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-17 20:56:48 +0700

    add_form

--- routes/forms.py
+++ routes/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from cities.models import City
+from routes.models import Route
+from trains.models import Train
 
 
 class RouteForm(forms.Form):    # Проверка соблюдения уникальности
@@ -17,3 +19,21 @@ class RouteForm(forms.Form):    # Проверка соблюдения уник
         'class': 'form-control',
         'placeholder': 'Время в пути'
     }))
+
+
+class RouteModelForm(forms.ModelForm):
+    name = forms.CharField(label='Название маршрута', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Введите название маршрута'
+    }))
+    from_city = forms.ModelChoiceField(label='Откуда', queryset=City.objects.all(), widget=forms.HiddenInput())
+    to_city = forms.ModelChoiceField(label='Куда', queryset=City.objects.all(), widget=forms.HiddenInput())
+
+    trains = forms.ModelMultipleChoiceField(label='Через города', queryset=Train.objects.all(), required=False, widget=forms.SelectMultiple(attrs={
+        'class': 'form-control d-none'}
+    ))
+    travel_time = forms.IntegerField(label='Время в пути', widget=forms.HiddenInput())
+
+    class Meta:
+        model = Route
+        fields = '__all__'
