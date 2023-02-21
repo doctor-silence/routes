53c2c3cd8acd6f92e8cc930824b71ceae7a2aa78
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-02-21 21:31:57 +0700

    add_acc_form

---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label='username', widget=forms.TextInput(attrs={  #Изменение формы
+        'class': 'form-control',
+        'placeholder': 'Введите Username'
+    }))
+
+    password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={  # Изменение формы
+        'class': 'form-control',
+        'placeholder': 'Введите password'
+    }))
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        if username and password:
+            qs = User.objects.filter(username=username)
+            if not qs.exists():
+                raise forms.ValidationError('Такого пользователя нет')
+            #Проверка зашифрованного пароля
+            if not check_password(password, qs[0].password):
+                raise forms.ValidationError('Неверный пароль')
+            #Проверка пользователя на активность
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError('Данный пользователь невктивен')
+        return super().clean(*args, **kwargs)
