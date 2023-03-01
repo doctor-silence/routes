e0eff53a5338d5031610f9834a7613cb44b0e93d
doctor-silence <vladimirkrizhanovski@gmail.com>
2023-03-01 09:26:19 +0700

    add_trains_app

--- trains/apps.py
+++ trains/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
+# Настройка отображения меню админки
 class TrainsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trains'
+    verbose_name = 'Поезда'
